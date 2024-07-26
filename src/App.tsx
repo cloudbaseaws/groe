e49f5031f47397cb27320aca1a053e41641856b2
Cloudbaseaws <161646401+cloudbaseaws@users.noreply.github.com>
2024-07-26 15:11:30 -0700

    Learning React in Amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-
-import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-
+//import Message from "./Message";
 const client = generateClient<Schema>();
 
 function App() {
+  //const client = generateClient<Schema>();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -19,37 +19,38 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
-  
-    function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
+  //function Message() {
+  //  return <div>Message/</div>;
+  //}
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
   }
   return (
     <Authenticator>
-      {({ signOut, user }) => (  
-    <main>
-      <h1>{user?.signInDetails?.loginId}'s Alaska</h1>
-      <h1>Alaska</h1>
-      <button onClick={createTodo}>+ Input </button>
-      <ul>
-        {todos.map((todo) => <li 
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>
-          {todo.content}
-        </li>)}
-      </ul>
-      <div>
-        🥳 Alaska. Create a New Item.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
-       
+      {({ signOut, user }) => (
+        <main>
+          <h1>{user?.signInDetails?.loginId}'s Alaska</h1>
+          <button onClick={createTodo}>+ Input </button>
+          <ul>
+            {todos.map((todo) => (
+              <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
+                {todo.content}
+              </li>
+            ))}
+          </ul>
+          <div>
+            🥳 Alaska. Create a New Item.
+            <br />
+            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+              Review next step of this tutorial.
+            </a>
+          </div>
+          <button onClick={signOut}>Sign out</button>
+        </main>
       )}
-      </Authenticator> 
-  )
+    </Authenticator>
+  );
 }
 
 export default App;
