5fee6a4696ac5892032cb52b733f1092edfa1c03
Cloudbaseaws <161646401+cloudbaseaws@users.noreply.github.com>
2024-08-21 14:40:17 -0700

    major upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,40 @@
 
-import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { useEffect, useState } from 'react';
+import { Schema, Model, Enum } from '@aws-amplify/datastore';
+import { generateClient } from 'aws-amplify/data';
+import UserStoryList from './components/UserStoryList';
 
-const client = generateClient<Schema>();
+// Define the UserStory model
+const UserStory = new Schema('UserStory', {
+  // ... your UserStory model fields
+});
+
+const client = generateClient<typeof UserStory>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [userStories, setUserStories] = useState([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+    const fetchUserStories = async () => {
+      const stories = await client.models.UserStory.list();
+      setUserStories(stories);
+    };
+    fetchUserStories();
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-  
-    function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
   return (
     <Authenticator>
-      {({ signOut }) => (  
-    <main>
-      <h1>Product Owner</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => <li 
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>
-          {todo.content}
-        </li>)}
-      </ul>
-      <div>
-        🥳 Alaska. Create a New Item.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
-       
+      {({ signOut }) => (
+        <main>
+          <h1>Product Backlog</h1>
+          <UserStoryList userStories={userStories} />
+          <button onClick={signOut}>Sign out</button>
+        </main>
       )}
-      </Authenticator> 
-  )
+    </Authenticator>
+  );
 }
 
 export default App;
